b11cbcbec7502d415fd28a97f151a759c55de14c
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2023-10-02 11:19:13 +0530

    Time: 4 ms (92.59%), Space: 9.7 MB (20.83%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-         int m = word1.size(), n = word2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        for (int i = 1; i <= m; i++) {
-            dp[i][0] = i;
-        }
-        for (int j = 1; j <= n; j++) {
-            dp[0][j] = j;
-        }
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (word1[i - 1] == word2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) + 1;
-                }
-            }
-        }
-        return dp[m][n];
+    int f(int i,int j,string &s1, string &s2,vector<vector<int>> &dp){
+        if(i<0)return j+1;
+        if(j<0)return i+1;
+        
+        if(dp[i][j]!=-1)return dp[i][j];
+        
+        int v1,v2,v3;
+        if(s1[i]==s2[j]) return dp[i][j] =  0 + f(i-1,j-1,s1,s2,dp);
+        
+        v1 = 1 + f(i,j-1,s1,s2,dp); // insert
+        v2 = 1 + f(i-1,j,s1,s2,dp); // delete
+        v3 = 1 + f(i-1,j-1,s1,s2,dp); // replace
+        
+       return dp[i][j] = min({v1,v2,v3});
+        
+    }
+    int minDistance(string s1, string s2) {
+        
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return f(n-1,m-1,s1,s2,dp);
     }
 };
