52ed7fe2c6709d8da37365cf55d5de6e948ee5cf
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2023-10-06 09:33:33 +0530

    Time: 128 ms (12.15%), Space: 45.6 MB (29.92%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -14,8 +14,26 @@ public:
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1 , -1));
-        int ans = fn(n-1 , amount , coins,dp );
-        return (ans >= 1e9) ? -1 : ans;
+        vector<vector<int>> dp(n, vector<int>(amount+1 , 0));
+        //int ans = fn(n-1 , amount , coins,dp );
+        //return (ans >= 1e9) ? -1 : ans;
+        for(int j=1;j<=amount;j++)
+        {
+            if(j%coins[0]==0)dp[0][j] = j/coins[0];
+            else
+            dp[0][j] = 1e6;
+        }
+        for(int idx=1;idx<n;idx++)
+        {
+            for(int j=1;j<=amount;j++)
+            {
+                int notTake = dp[idx-1][j];
+                int take = 1e6;
+                if(coins[idx]<=j)take = 1 + dp[idx][j-coins[idx]];
+                
+                dp[idx][j] = min(take,notTake);
+            }
+        }
+        return dp[n-1][amount]==1e6?-1:dp[n-1][amount];
     }
 };
