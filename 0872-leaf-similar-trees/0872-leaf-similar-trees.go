4efb578905888d911bcece96e89a1eb05f447061
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-01-09 20:36:38 +0530

    Time: 2 ms (71.85%), Space: 2.5 MB (52.52%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.go
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.go
@@ -1,39 +1,39 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
  * }
  */
-class Solution {
-     public void dfs(TreeNode root, ArrayList<Integer> al )
-     {
-         if(root==null)return;
-             
-          if(root.left==null && root.right == null)
-          {
-              al.add(root.val);
-          }
-         dfs(root.left,al);
-         dfs(root.right,al);
-         
-         
-     }
-    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        ArrayList<Integer> al1= new ArrayList<Integer>();
-         ArrayList<Integer> al2 = new ArrayList<Integer>();
-        dfs(root1,al1);
-        dfs(root2,al2);
-       // boolean ans = al1==al2;
-       // System.out.println(al1 + "al1" +al2 + "al2");
-        return al1.equals(al2);
+func leafSimilar(root1 *TreeNode, root2 *TreeNode) bool {
+    var arr1, arr2 []int
+    
+    dfs(root1, &arr1)
+    dfs(root2, &arr2)
+    
+    if len(arr1) != len(arr2) {
+        return false
     }
+    
+    for i := 0; i < len(arr1); i++ {
+        if arr1[i] != arr2[i] {
+            return false
+        }   
+    }
+    
+    return true
+}
+
+func dfs(root *TreeNode, arr *[]int) {
+    if root == nil {
+        return 
+    }
+    
+    if root.Left == nil && root.Right == nil {
+        *arr = append(*arr, root.Val)
+    }
+    
+    dfs(root.Left, arr)
+    dfs(root.Right, arr)
 }
