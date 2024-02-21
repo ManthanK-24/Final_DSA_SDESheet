5afc4e097f5bdcf42c2ae3352d09e2a46eebd174
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-02-21 15:04:40 +0530

    Time: 5 ms (16.34%), Space: 7 MB (52.16%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        
+        uint32_t ans = 0, msk = 1<<31;
+        while(n)
+        {
+            if(n&1) ans += msk;
+            n >>= 1;
+            msk >>= 1;
+        }
+        return ans;
+    }
+};
