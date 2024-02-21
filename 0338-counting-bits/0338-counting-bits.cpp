fe56875dd69a8cd7c7ca6ecc5a110918c69c3835
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-02-21 14:39:07 +0530

    Time: 2 ms (81.17%), Space: 8.9 MB (49.59%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -3,9 +3,15 @@ public:
     vector<int> countBits(int n) {
         
         vector<int> ans(n+1,0);
-        for(int i=0;i<=n;i++)
+        
+        // x and y=x/2 
+        // noOfsetBits(x) - noOfsetBits(y) <= 1
+        // if x is odd then it will have 1 extra set bit than y
+        
+        for(int i=1;i<=n;i++)
         {
-            ans[i] = __builtin_popcount(i);
+            // ans[i] = __builtin_popcount(i);
+            ans[i] = ans[i/2] + i%2;
         }
         return ans;
     }
