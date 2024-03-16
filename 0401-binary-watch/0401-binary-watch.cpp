52fdc191ff2cdc2270154c43ed649224f3dcff35
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-03-16 14:47:42 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (79.11%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int n) {
+        
+        vector<string> ans;
+       
+        for(int h=0;h<12;h++)
+        {
+            for(int m=0;m<60;m++)
+            {
+                if(__builtin_popcount(h) + __builtin_popcount(m) == n)
+                {
+                    ans.push_back(to_string(h)+(m<10?":0":":")+to_string(m));
+                }
+            }
+        }
+        return ans;
+    }
+};
