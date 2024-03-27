8f32505dbbef261281e78195acd0a9a6abc6e903
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-03-27 15:11:17 +0530

    Time: 11 ms (34.99%), Space: 22 MB (5.48%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int j = 0;
+        
         map<int,int> freq;
-
+        
+        int j = 0;
         for(int i=0;i<nums.size();i++)
         {
             if(freq[nums[i]]==0)
             {
-                freq[nums[i]]++;
-                nums[j++]=nums[i];
+               freq[nums[i]]++;
+                nums[j++] = nums[i];
             }
             
         }
-        return j;
+        
+        return freq.size();
     }
 };
