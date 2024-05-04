5679851341b4d3487fe78119b4237ece3c1658a0
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-05-04 23:23:03 +0530

    Time: 3 ms (39.13%), Space: 7.2 MB (93.89%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -10,12 +10,14 @@ public:
     int num2 = 0;
     while(i<n1 || j<n2)
     {
-        while(i<n1 && version1[i]!='.'){
+        while(i<n1 && version1[i]!='.')
+        {
             num1 = num1*10+(version1[i]-'0');
             i++;
         }
         
-        while(j<n2 && version2[j]!='.'){
+        while(j<n2 && version2[j]!='.')
+        {
             num2 = num2*10+(version2[j]-'0');;
             j++;
         }
