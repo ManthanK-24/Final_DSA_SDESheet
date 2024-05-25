8b9f90abd6f1035bd5b73cfc14b653855174538d
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-05-25 11:16:30 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (87.09%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,61 +1,40 @@
 class Solution {
 public:
-    void solve(string s, vector<string>& res, unordered_set<string>& st, vector<string>&temp)
+
+void solve(int idx, string &s, unordered_set<string>& st,vector<string>& ans,
+                vector<string>& tmp)
+{
+    if(idx==s.size())
     {
-        if(s.length() == 0){
-            string str = "";
-            for(auto it:temp){
-                str += it + " ";
-            }
-            str.pop_back();
-            res.push_back(str);
-            return;
+        string sentence;
+        for(auto it:tmp)
+        {
+            sentence += it;
+            sentence += " ";
         }
-        for(int i=0;i<s.length(); i++){
-            if(st.count(s.substr(0, i+1))){
-                temp.push_back(s.substr(0, i+1));
-                solve(s.substr(i+1), res, st, temp);
-                temp.pop_back();
-            }
+        sentence.pop_back();
+        ans.push_back(sentence);
+       return;
+    }
+    string x;
+    for(int i=idx;i<s.size();i++)
+    {
+        x += s[i];
+        if(st.find(x)!=st.end())
+        {
+            tmp.push_back(x);
+            solve(i+1,s,st,ans,tmp);
+            tmp.pop_back();
         }
     }
-    vector<string> wordBreak(string s, vector<string>& wordDict) {
-        vector<string>res, temp;
-        unordered_set<string>st(wordDict.begin(), wordDict.end());
-        
-        solve(s, res, st, temp);
-        return res;
-    }
-    /*void solve(int idx,string s,map<string,bool> mp,string& tmp,vector<string>& ds)
-    {
-        cout<<idx<<"idx "<<tmp<<"tmp\n";
-        if(idx==s.size())
-        {
-            ds.push_back(tmp);
-        }
-        
-        for(int i=idx;i<s.size();i++)
-        {
-            tmp += s[i];
-            cout<<tmp<<"$tmp "<<i<<"i #\t";
-            if(mp.find(tmp)!=mp.end())
-            {
-                tmp += " ";
-                solve(i+1,s,mp,tmp,ds);
-            }
-           // tmp.pop_back();
-        }
-    }
-    vector<string> wordBreak(string s, vector<string>& wordDict) 
-    {
-        vector<string> ds;
-        map<string,bool> mp;
-        for(auto x:wordDict)
-        {
-            mp[x]=1;
-        }
-        string tmp;
-        solve(0,s,mp,tmp,ds);
-        return ds;
-    }*/
+}
+vector<string> wordBreak(string &s, vector<string> &dictionary)
+{
+    // Write your code here
+    vector<string> ans;
+    vector<string> tmp;
+    unordered_set<string> st(dictionary.begin(),dictionary.end());
+    solve(0,s,st,ans,tmp);
+    return ans;
+}
 };
