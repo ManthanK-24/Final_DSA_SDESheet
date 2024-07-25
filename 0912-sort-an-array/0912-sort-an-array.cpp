85f51a10cea203102f577217fb48f0a178ac8624
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-07-25 06:54:59 +0530

    Time: 108 ms (74.36%), Space: 71.5 MB (60.44%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,37 +1,45 @@
 class Solution {
 public:
-    void merge(vector<int>& ar, int l, int m ,int r)
+    void merge(vector<int>& nums, vector<int>& tmp,int lft,int mid, int rgt)
     {
-        int i = l, j = m+1, k =0;
-        vector<int> tmp; //(r-l+1,0);
-        while(i<=m && j<=r)
+        int i=lft,j=mid+1,k=lft;
+        while(i<=mid && j<=rgt)
         {
-            if(ar[i]>ar[j])
+            if(nums[i]<=nums[j])
             {
-                tmp.push_back(ar[j++]);
+                tmp[k++]=nums[i++];
             }
             else
             {
-                tmp.push_back(ar[i++]);
+                tmp[k++]=nums[j++];
             }
         }
-        while(i<=m)tmp.push_back(ar[i++]);
-        while(j<=r)tmp.push_back(ar[j++]);
-        for(int x=l;x<=r;x++)
-            ar[x] = tmp[x-l];
+        while(i<=mid)
+        {
+            tmp[k++]=nums[i++];
+        }
+        while(j<=rgt)
+        {
+            tmp[k++]=nums[j++];
+        }
+        for(int x=lft;x<=rgt;x++)
+        {
+            nums[x]=tmp[x];
+        }
         
     }
-    void ms(vector<int>& ar,int l, int r)
+    void mergesort(vector<int>& nums, vector<int>& tmp,int lft, int rgt)
     {
-        if(l>=r)return;
-        int m = l + (r-l)/2;
-        ms(ar,l,m);
-        ms(ar,m+1,r);
-        merge(ar,l,m,r);
+        if(lft>=rgt)return;
+        int mid = lft + (rgt-lft)/2;
+        mergesort(nums,tmp,lft,mid);
+        mergesort(nums,tmp,mid+1,rgt);
+        merge(nums,tmp,lft,mid,rgt);
     }
-    vector<int> sortArray(vector<int>& ar) {
+    vector<int> sortArray(vector<int>& nums) {
         
-        ms(ar,0,ar.size()-1);
-        return ar;
+        vector<int> tmp = nums;
+        mergesort(nums,tmp,0,nums.size()-1);
+        return nums;
     }
 };
