ff97b874bd4d6ac9d50ac4bc2d3503ee12f990aa
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-09-21 07:00:53 +0530

    Time: 7 ms (75.99%), Space: 14.1 MB (51.90%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<int> lexicalOrder(int n) {
         
-        vector<int> ans(n,0);
-        for(int i=0;i<n;i++)ans[i] = i+1;
+//         vector<int> ans(n,0);
+//         for(int i=0;i<n;i++)ans[i] = i+1;
         
-        sort(ans.begin(),ans.end(),
-            [](int& a, int& b){return to_string(a)<to_string(b);});
-        return ans;
+//         sort(ans.begin(),ans.end(),
+//             [](int& a, int& b){return to_string(a)<to_string(b);});
+//         return ans;
         
-        // vector<int> res;
-        // helper(1,n,res);
-        // return res;
+        vector<int> res;
+        helper(1,n,res);
+        return res;
     }
     void helper(int target, int n, vector<int>& res)
     {
