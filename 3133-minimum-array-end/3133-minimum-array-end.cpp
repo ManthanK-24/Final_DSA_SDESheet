0e1df5e4e0676a2522b4ed0a5d12018a35b78358
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-11-09 20:43:39 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (82.17%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.cpp
+++ 3133-minimum-array-end/3133-minimum-array-end.cpp
@@ -1,30 +1,19 @@
-
-# define ll long long 
-
 class Solution {
 public:
-    ll minEnd(int n, int x) 
-    {
-        ll ans = 0;
-        ll nn = n;
-        ll xx =  x;
+    long long minEnd(int n, int x) {
+         long result = x;
+        long remaining = n - 1;
+        long position = 1;
         
-        nn--;
-        ll p = 0 ;
-        for(ll i=0;i<63;i++)
-        {
-            if(xx&1LL<<i)
-            {
-                ans |= 1LL<<i;
+        while (remaining) {
+            if (!(x & position)) {
+                result |= (remaining & 1) * position;
+                remaining >>= 1;
             }
-            else
-            {
-                ans |= ((nn>>p)&1LL)<<i;
-                p++;
-            }
+            position <<= 1;
         }
-        return ans;
         
-       
+        return result;
     }
+    
 };
