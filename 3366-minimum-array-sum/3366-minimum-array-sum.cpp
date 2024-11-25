da7e02c00d4635e1baf265b40f983304d0034e7f
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-11-25 18:40:49 +0530

    Time: 148 ms (66.67%), Space: 84.3 MB (33.33%) - LeetHub

--- 3366-minimum-array-sum/3366-minimum-array-sum.cpp
+++ 3366-minimum-array-sum/3366-minimum-array-sum.cpp
@@ -1,56 +1,62 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int k, vector<vector<vector<int>>>& dp, int idx, int op1, int op2) {
-        // Base Case: If we reach the end of the array
-        if (idx == nums.size())
-            return 0;
-
-        // If result is already computed, return it
-        if (dp[idx][op1][op2] != -1)
-            return dp[idx][op1][op2];
-
+    int solve(vector<int>& nums, vector<vector<vector<int>>>& dp, int idx,int k, int op1, int op2)
+    {
+        if(idx==nums.size())return 0;
+        
+        if(dp[idx][op1][op2]!=-1)return dp[idx][op1][op2];
+        
         int ans = INT_MAX;
+        // five cases
+        // 1. Only op1
+        // 2. Only op2
+        // 3. Op1Op2
+        // 4. Op2Op1
+        // 5. None
 
-        // CASE 1: Perform only op1
-        if (op1 != 0) {
-            int sumOp1 = ceil(nums[idx] / 2.0) + solve(nums, k, dp, idx + 1, op1 - 1, op2);
-            ans = min(ans, sumOp1);
+        //  1. Only Op1
+        if(op1>0)
+        {
+        int sumOp1 = ceil(nums[idx]/2.0) + solve(nums,dp,idx+1,k,op1-1,op2);
+        ans = min(ans,sumOp1);
         }
-
-        // CASE 2: Perform only op2
-        if (op2 != 0 && nums[idx] >= k) {
-            int sumOp2 = (nums[idx] - k) + solve(nums, k, dp, idx + 1, op1, op2 - 1);
-            ans = min(ans, sumOp2);
+        
+        // 2. Only Op2
+        if(op2>0 && nums[idx]>=k)
+        {
+            int sumOp2 = nums[idx]-k + solve(nums,dp,idx+1,k,op1,op2-1);
+            ans = min(ans,sumOp2);
         }
-
-        // CASE 3 and CASE 4: Perform both op1 and op2 in different orders
-        if (op1 != 0 && op2 != 0) {
-            int afterOp1 = ceil(nums[idx] / 2.0);
-            if (afterOp1 >= k) {
-                // CASE 3: Perform op1 followed by op2
-                int sumOp1Op2 = (afterOp1 - k) + solve(nums, k, dp, idx + 1, op1 - 1, op2 - 1);
-                ans = min(ans, sumOp1Op2);
+        if(op1>0 && op2>0)
+        {
+            int afterOp1 = ceil(nums[idx]/2.0);
+            if(afterOp1>=k)
+            {
+                // 3. Op1Op2
+                int sumOp1Op2 = afterOp1-k + solve(nums,dp,idx+1,k,op1-1,op2-1);
+                ans = min(ans,sumOp1Op2);
             }
-            if (nums[idx] >= k) {
-                // CASE 4: Perform op2 followed by op1
-                int sumOp2Op1 = ceil((nums[idx] - k) / 2.0) + solve(nums, k, dp, idx + 1, op1 - 1, op2 - 1);
-                ans = min(ans, sumOp2Op1);
+            if(nums[idx]>=k)
+            {
+                // 4. Op2Op1
+                int sumOp2Op1 = ceil((nums[idx]-k)/2.0) + solve(nums,dp,idx+1,
+                                                                k,op1-1,op2-1);
+                ans = min(ans,sumOp2Op1);
             }
         }
-
-        // CASE 5: Perform no operation
-        int sumNoOp = nums[idx] + solve(nums, k, dp, idx + 1, op1, op2);
-        ans = min(ans, sumNoOp);
-
-        // Store the result in dp table
+        // 5. None
+        int sumNone = nums[idx] + solve(nums,dp,idx+1,k,op1,op2);
+        
+        ans = min(ans,sumNone);
+        
         return dp[idx][op1][op2] = ans;
+        
+        
+        
     }
-
     int minArraySum(vector<int>& nums, int k, int op1, int op2) {
-        // Initialize dp with -1
-        vector<vector<vector<int>>> dp(nums.size(), vector<vector<int>>(op1 + 1, vector<int>(op2 + 1, -1)));
-
-        // Solve the problem
-        return solve(nums, k, dp, 0, op1, op2);
+        
+        vector<vector<vector<int>>> dp(nums.size(),vector<vector<int>>(op1+1,vector<int>(op2+1,-1)));
+        return solve(nums,dp,0,k,op1,op2);
     }
 };
