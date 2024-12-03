ee0acce7989f813b86fc8c443a7075ab16bc3ba4
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-12-03 17:39:46 +0530

    Time: 87 ms (11.37%), Space: 64.2 MB (6.71%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
@@ -1,29 +1,50 @@
 class Solution {
 public:
-    int countNicePairs(vector<int>& nums) {
-     
-        // Brute TC:O(n*n*9) SC:O(1)
-        // Optimised TC:O(n) SC:O(n)
-        
-        int ans = 0;
-        int mod = 1e9+7;
-        unordered_map<int,int> dif;
-        int n = nums.size();
-        
-        string tmp;
-        int rev = 0;
-        
-        for(int i=0;i<n;i++)
+    int reverseNum(int n){
+        if(n==0)return 0;
+        string s = to_string(n);
+        reverse(s.begin(),s.end());
+        // 1000
+        // 0001
+        string ans;
+        int idx = -1;
+        for(int i=0;i<s.size();i++)
         {
-             tmp = to_string(nums[i]);
-             reverse(tmp.begin(),tmp.end());
-             rev = stoi(tmp);
-            //cout<<rev<<"rev ";
-             ans = (ans+dif[nums[i]-rev]++)%mod;
-             //dif[max(nums[i],rev)-min(nums[i],rev)]++;
-              
+            if(s[i]!='0')
+            {
+                idx = i;
+                break;
+            }
         }
-    //    cout<<"\n";
-        return ans;
+        for(int i=idx;i<s.size();i++)
+        {
+            ans += s[i];
+        }
+        
+        return stoi(ans);
+        
+    }
+    int countNicePairs(vector<int>& nums) {
+        
+        // if nums[i] - rev(nums[i]) == nums[j] - rev(nums[j])
+        int cnt = 0;
+        int mod = 1e9+7;
+        map<long int, long int> mpFreq;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            long int tmp = nums[i] - reverseNum(nums[i]);
+            mpFreq[tmp]++;
+        }
+        
+        for(auto [x,y]:mpFreq)
+        {
+            if(y>1)
+            {
+                long int tmp = 1LL*y*(y-1)/2;
+                cnt = (cnt + tmp)%mod;
+            }
+        }
+        return cnt;
     }
 };
