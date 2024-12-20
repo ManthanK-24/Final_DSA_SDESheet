1bd340b8aeff199d8c198cb7cccb41fa890f7b9c
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-12-20 18:05:08 +0530

    Time: 0 ms (100.00%), Space: 62.9 MB (22.36%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,21 +1,51 @@
 class Solution {
 public:
-    int maxArea(vector<int>& height)
-    {
-          int area = 0;
+    int maxArea(vector<int>& height) {
         
-          int i=0, j= size(height)-1;
-          
-          while(i<j)
-          {
-              int tmp = min(height[i],height[j]) * (j-i);
-              area = max(area, tmp);
-              
-              if(height[i]<height[j])
-                  i++;
-              else
-                  j--;
-          }
-        return area;
+        int mxArea = 0;
+        int i=0, j =height.size()-1;
+        while(i<=j)
+{
+    // compute Area for each (i,j)
+    int h = min(height[i],height[j]), w = j-i;
+    mxArea = max(mxArea, h*w);
+        
+    // 8,7 
+    
+    if(height[i]>height[j])
+    {
+        
+        j--;
+    }
+    else
+    {
+        i++;
+    }
+}
+//         for(int i=0;i<height.size();i++)
+//         {
+//             for(int j=i+1;j<height.size();j++)
+//             {
+                
+//                 // i to j: 
+//                 int w = j-i, h = min(height[i],height[j]);
+//                 int area = w*h;
+//                 mxArea = max(mxArea, area);
+//             }
+//         }
+        return mxArea;
     }
 };
+
+//i=0; => [(1,0)]
+//i=1; 8,1 [(8,1)] => 7
+//i=2; 6,2 [(8,1)] => 6
+// i=3 2,3 [(8,1)] => 4
+// i=4 5,4 [(8,1)] => 15
+// i=5 4,5 [(8,1)] => 16
+// i=6 9,6 [(9,6)] => 40
+// i=7 3,7 [(9,6)] => 3
+// i=8 7,8 [(9,6)] => 14
+
+
+
